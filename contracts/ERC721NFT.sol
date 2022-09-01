784c232c8871d94859a0aaab69d280715b3cdcb8
Memo Chou <memochou1993@gmail.com>
2022-09-02 00:03:43 +0800

    Update contract

--- contracts/ERC721NFT.sol
+++ contracts/ERC721NFT.sol
@@ -1,17 +1,18 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.13;
 
-import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/utils/Strings.sol";
 
-contract ERC721NFT is ERC721Enumerable, Ownable {
+contract ERC721NFT is ERC721URIStorage, Ownable {
     mapping(address => uint8) balances;
 
     uint256 tokenCount = 0;
 
     constructor() ERC721("ERC721NFT", "NFT") {}
 
-    function mintNFT()
+    function mintNFT(string memory _tokenURI)
         public
         returns (uint256)
     {
@@ -19,6 +20,7 @@ contract ERC721NFT is ERC721Enumerable, Ownable {
         balances[msg.sender]++;
         tokenCount++;
         _mint(msg.sender, tokenCount);
+        _setTokenURI(tokenCount, string(abi.encodePacked(_tokenURI, Strings.toString(tokenCount))));
         return tokenCount;
     }
 }
--- src/main.js
+++ src/main.js
@@ -4,28 +4,17 @@ import { abi } from '../build/contracts/ERC721NFT.json';
 const { VITE_CONTRACT_ADDRESS } = import.meta.env;
 
 const render = async () => {
-  list.textContent = '';
-  const balance = await contract.balanceOf(account);
-  const iteratee = (i) => contract.tokenOfOwnerByIndex(account, i);
-  const promises = [...Array(Number(balance)).keys()].map(iteratee);
-  const tokens = await Promise.all(promises);
-  tokens.forEach((token) => {
-    const image = document.createElement('img');
-    image.src = getTokenURI(token);
-    list.appendChild(image);
-  });
+  //
 };
 
 const mintNFT = async () => {
-  const res = await contract.mintNFT();
+  const tokenURI = 'https://github.com/memochou1993/nft-leopard-cat-images/tree/main/output/metadata/';
+  const res = await contract.mintNFT(tokenURI);
   await res.wait();
   render();
 };
 
-const getTokenURI = (token) => `https://github.com/memochou1993/nft-leopard-cat-images/blob/main/output/${token}.png?raw=true`;
-
 const button = document.getElementById('mint-nft');
-const list = document.getElementById('token-list');
 
 const provider = new ethers.providers.Web3Provider(window.ethereum);
 const [account] = await provider.send('eth_requestAccounts');
